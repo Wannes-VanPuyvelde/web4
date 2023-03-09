349c41bd55a363be9a10ac460e903658ff4306bf
Wannes-VanPuyvelde <r0704704@ucll.be>
2023-03-09 16:30:59 +0100

    Created model files

---
+++ backend/domain/model/Account.ts
@@ -0,0 +1,12 @@
+class Account{
+    id: number
+    username: string
+    email: string
+    password? : string
+
+    constructor(username: string, email:string, password?: string){
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+}
